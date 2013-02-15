6a363b9acca9f3baa0ff42b82eb4f3b55f94d4e8
Jacek Podkanski <ruby.object@googlemail.com>
2013-02-15 21:57:15 +0000

    finished refactoring

--- app/models/estate.rb
+++ app/models/estate.rb
@@ -5,22 +5,22 @@ class Estate < ActiveRecord::Base
       self.due_dates.split( ',').collect do |d| d.strip end
   end
   
-  def remind(date)
+  def remind(date,estates)
     date = Date.parse date
     results = []
-    Estate.all.each do |e|
-      e.due_dates_list.each do |due_date|
-        d = Date.parse "#{due_date} #{date.year}"
-        d = d + 1.year if d < date #due dates must be in future
+    estates.each do |e|
+      e.due_dates_list.each do |db_due_date|
+        due_date = Date.parse "#{db_due_date} #{date.year}"
+        due_date = due_date + 1.year if due_date < date #due dates must be in future
         if e.service_charge_period == "Quarterly"          
-          rd = d - 1.month
+          reminder_date = due_date - 1.month
         elsif e.service_charge_period == "Twice a year"
-          rd = d - 2.months
+          reminder_date = due_date - 2.months
         else
           raise "wrong service charge period in the database"
         end
-        if date.between? rd, d
-          results << "#{e.estate_code} due date #{due_date} #{d.year}"
+        if date.between? reminder_date, due_date
+          results << "#{e.estate_code} due date #{db_due_date} #{due_date.year}"
         end
       end
     end
