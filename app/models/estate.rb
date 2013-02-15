e85a0f27771ff89114fbee2952e105b9a6abb077
Jacek Podkanski <ruby.object@googlemail.com>
2013-02-15 18:59:58 +0000

    more code cleanup

--- app/models/estate.rb
+++ app/models/estate.rb
@@ -7,10 +7,10 @@ class Estate < ActiveRecord::Base
   
   def remind(date)
     date = Time.parse date
-    year = date.year
+    results = []
     Estate.all.each do |e|
       e.due_dates_list.each do |due_date|
-        d = Time.parse "#{due_date} #{year}"
+        d = Time.parse "#{due_date} #{date.year}"
         d = d + 1.year if d < date #due dates must be in future
         if e.service_charge_period == "Quarterly"          
           rd = d - 1.month
@@ -20,11 +20,11 @@ class Estate < ActiveRecord::Base
           raise "wrong service charge period in the database"
         end
         if rd <= date and date <= d
-          puts "#{e.estate_code} due date #{due_date} #{d.year}"
+          results << "#{e.estate_code} due date #{due_date} #{d.year}"
         end
       end
     end
-    ''
+    results
   end
 
 end
