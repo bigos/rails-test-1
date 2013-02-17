f9b6c1b5910cc697ab79846c64e601d9dd8e905b
Jacek Podkanski <ruby.object@googlemail.com>
2013-02-17 22:11:56 +0000

    added validation

--- app/models/estate.rb
+++ app/models/estate.rb
@@ -1,6 +1,8 @@
 class Estate < ActiveRecord::Base
   attr_accessible :due_dates, :estate_code, :service_charge_period
 
+  validates_uniqueness_of :estate_code
+
   def due_dates_list
       self.due_dates.split( ',').collect do |d| d.strip end
   end
